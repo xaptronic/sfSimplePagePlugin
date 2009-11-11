b54267a82fef8ba0db8fb6b84a21e91518820a7d
basos <basos@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-11-11 02:26:10 +0000

    Seperate route prefix from data dir, new configuration from app.yml more precise 404 error
    git-svn-id: http://svn.symfony-project.com/plugins/sfSimplePagePlugin/trunk@23772 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- modules/sfSimplePage/lib/BasesfSimplePageActions.class.php
+++ modules/sfSimplePage/lib/BasesfSimplePageActions.class.php
@@ -13,17 +13,20 @@ abstract class BasesfSimplePageActions extends sfActions
   public function executeIndex()
   {
     // set template_path from path_info
-    $base = sfConfig::get("mod_sfsimplepage_template_path");
-    $pathinfo = ($this->getRequest()->getPathinfo() == "/")? $base . "/" : $this->getRequest()->getPathinfo();
-    if (!preg_match("#^{$base}#", $pathinfo)) {
-      $pathinfo = $base . $pathinfo;
-    }
-    $template_path = sfConfig::get('sf_data_dir') . DIRECTORY_SEPARATOR . ltrim($pathinfo,'/');
+    $pathinfo = $this->getRequest()->getPathInfo() ;
+    $route_prefix = sfConfig::get("app_sf_simple_page_plugin_route_prefix") ;
+
+    //remove  trailing slashes
+    $route_prefix = '/'. trim( $route_prefix, '/' ) ;
+    $pathinfo = preg_replace( "#^{$route_prefix}#", "", $pathinfo ) ;
+
+    $template_path = sfConfig::get("app_sf_simple_page_plugin_template_path") . DIRECTORY_SEPARATOR . ltrim($pathinfo,'/');
     // check whether file is ended with directory separator or not
     // if ended with '/', add "index.html"
     if (preg_match("#/$#Di", $template_path)) {
       $template_path .= "index.html";
     }
+       
     // check template file
     $in = dirname($template_path);
     $name = basename($template_path);
@@ -32,7 +35,7 @@ abstract class BasesfSimplePageActions extends sfActions
              ->name($name)
              ->ignore_version_control()
              ->in($in);
-    $this->forward404unless(count($files)==1);
+    $this->forward404unless(count($files)==1, "File not found ". $name );
     // set template file path
     $this->include_file_path = $files[0];
     return sfView::SUCCESS;
