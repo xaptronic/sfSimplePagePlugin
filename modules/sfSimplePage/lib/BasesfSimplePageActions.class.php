976250f7968816745c3045408b1cfa6aa10ca0a1
basos <basos@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-12-08 16:48:24 +0000

    Added a fallback check when i18n is enabled, if the file is not found under the culture dir a final attemp to locate it is made in the static folder. This to accound for the case when one needs a file to be common to all languages. It avoids to copy the same file all over.
    git-svn-id: http://svn.symfony-project.com/plugins/sfSimplePagePlugin/trunk@25087 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- modules/sfSimplePage/lib/BasesfSimplePageActions.class.php
+++ modules/sfSimplePage/lib/BasesfSimplePageActions.class.php
@@ -22,12 +22,15 @@ abstract class BasesfSimplePageActions extends sfActions
 
     //check i18n support
     $culture = '' ;
+    $cult_template_path = '' ;
+    $gen_template_path = sfConfig::get("app_sf_simple_page_plugin_template_path", "static") . DIRECTORY_SEPARATOR . ltrim($pathinfo,'/');     
     if ( sfConfig::get('app_sf_simple_page_plugin_use_i18n', false ) ) {
         $culture = $this->getUser()->getCulture() ;
+        $cult_template_path = sfConfig::get("app_sf_simple_page_plugin_template_path", "static") . DIRECTORY_SEPARATOR . 
+            $culture . DIRECTORY_SEPARATOR . ltrim($pathinfo,'/');
     }
     
-    $template_path = sfConfig::get("app_sf_simple_page_plugin_template_path", "static") . DIRECTORY_SEPARATOR . 
-            ( (boolean)$culture ? $culture . DIRECTORY_SEPARATOR : '' ) . ltrim($pathinfo,'/');
+    $template_path =  ( (boolean)$culture ? $cult_template_path  : $gen_template_path ) ;
     // check whether file is ended with directory separator or not
     // if ended with '/', add "index.html"
     if (preg_match("#/$#Di", $template_path)) {
@@ -42,6 +45,19 @@ abstract class BasesfSimplePageActions extends sfActions
              ->name($name)
              ->ignore_version_control()
              ->in($in);
+    
+    // if i18n files were not found give it another try in the template path
+    if ( (boolean)$culture ) {
+        if ( !count($files) ) {
+            $in = dirname($gen_template_path);
+            $name = basename($gen_template_path);
+            $files = sfFinder::type('file')
+                     ->maxdepth(0)
+                     ->name($name)
+                     ->ignore_version_control()
+                     ->in($in);
+        }
+    }
     $this->forward404unless(count($files)==1, "File not found ". $name );
     // set template file path
     $this->include_file_path = $files[0];
